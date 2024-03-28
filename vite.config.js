794b80f7f844dd1fbd45ad0084536a5751bd4564
bryandino673 <bdino673@gmail.com>
2024-03-28 07:59:34 +0100

    First Commit

---
+++ vite.config.js
@@ -0,0 +1,52 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true,
+        globPatterns: ['**/*.{js,css,html,png,svg,ico}']
+      },
+      devOptions: {
+        enabled: true
+      },
+      injectRegister: 'auto',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'PWA',
+        short_name: 'PWA',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: './public/icon36x36.png',
+            sizes: '36x36',
+            type: 'image/png'
+          },
+          {
+            src: './public/icon48x48.png',
+            sizes: '48x48',
+            type: 'image/png'
+          },
+          {
+            src: './public/icon310x310.png',
+            sizes: '310x310',
+            type: 'image/png',
+            purpose: 'any '
+          },
+          {
+            src: './public/icon144x144.png',
+            sizes: '144x144',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    })
+  ]
+});
